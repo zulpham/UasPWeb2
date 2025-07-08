047190c8763273f8fdca173a44416587b47c70e4
zulpham <zufanlen4@gmail.com>
2025-07-08 21:04:03 +0700

    menambahkan views

--- app/Controllers/Ikanku.php
+++ app/Controllers/Ikanku.php
@@ -3,15 +3,15 @@ namespace App\Controllers;
 
 class Ikanku extends BaseController{
     public function index(): string{
-        return view('welcome_message');
+        return view('ikanku/index');
     }
     public function tentangkami(): string{
-        return view('tentangkami');
+        return view('ikanku/tentangkami');
     }
     public function acara(): string{
-        return view('acara');
+        return view('ikanku/acara');
     }
     public function kontak(): string{
-        return view('kontak');
+        return view('ikanku/kontak');
     }
 }
---
+++ app/Views/ikanku/tentangkami.php
@@ -0,0 +1,1 @@
+<h1>tentangkami</h1>
